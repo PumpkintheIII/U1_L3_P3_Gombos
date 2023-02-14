e52e0f955cd9a553be9438dee8bfde14b0d2a5eb
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-14 14:27:11 +0000

    Iteration 1

--- .ccls-cache/@home@runner@U1L3P3Gombos/main.c
+++ .ccls-cache/@home@runner@U1L3P3Gombos/main.c
@@ -3,9 +3,24 @@
   Seconds to Hours, Minutes, and Seconds
   Max Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //import input and output functions
 
-int main(void) {
+int main(void) { //define main function
+  int inputSeconds, hours, minutes, seconds; 
+  //initialize variable as interger
+
+  printf("This program allows you to enter an amount of seconds in whole numbers and the program will convert them to minutes. You will get one prompt:\n\t- Seconds: Please enter your seconds here\n"); //instructions
+  
+  printf("Seconds: "); //ask user for input
+  scanf("%d", &inputSeconds); //accept user input and format as int
+
+  minutes = inputSeconds/60; //convert seconds to minutes
+  hours = minutes/60; //convert mintues to hours
+  minutes = minutes%60; //find remaining minutes
+  seconds = inputSeconds - ((minutes * 60) + ((hours*60)*60));
+  //find remaining seconds
+
+  printf("%d seconds is equal to %d hours %d minutes and %d seconds.", inputSeconds, hours, minutes, seconds); //print output
   
   return 0;
 }
